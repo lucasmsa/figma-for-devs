71fb440d3d770d3bb38be03e45c51bb2b8224bfc
Lucas Moreira <lucasmsea@outlook.com>
2023-03-18 20:30:54 -0300

    feat: add js design tokens

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 18 Mar 2023 23:21:05 GMT
+ */
+
+module.exports = {
+  ColorPink500: "#db2777",
+  ColorGray600: "#a8a29e",
+  ColorGray700: "#57534e",
+  ColorGray800: "#292524",
+  ColorGray900: "#1c1917",
+  ColorWhite900: "#ffffff"
+};
